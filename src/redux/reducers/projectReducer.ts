53a56e94f7f32d15bac293937885f4b85b0fb50e
bondyaroslav <bondy2526@gmail.com>
2024-06-15 23:16:59 +0300

    Add projectReducer

---
+++ src/redux/reducers/projectReducer.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export interface Project {
+    id: number,
+    name: string,
+    description: string,
+    startDate: string,
+    endDate: string
+}
+
+interface InitialProjectState {
+    projects: Project[]
+}
+
+const initialState: InitialProjectState = {
+    projects: []
+}
+
+const projectSlice = createSlice({
+    name: "project",
+    initialState: initialState,
+    reducers: {
+        setProjects(state, action) {
+            state.projects = action.payload
+        },
+        addProject(state, action) {
+            state.projects.push(action.payload)
+        },
+        updateProject(state, action) {
+            const index = state.projects.findIndex(project => project.id === action.payload.id)
+            if (index !== -1) {
+                state.projects[index] = action.payload
+            }
+        }
+    }
+})
+
+export const { setProjects, addProject, updateProject } = projectSlice.actions
+
+export default projectSlice
